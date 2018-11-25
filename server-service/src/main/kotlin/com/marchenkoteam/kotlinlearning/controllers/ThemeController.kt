0d7e8eeadd845d6500596c63c984889cd729dbd5
kiseru <kis.alex97@gmail.com>
2018-11-25 17:53:04 +0300

    Created Theme API

---
+++ server-service/src/main/kotlin/com/marchenkoteam/kotlinlearning/controllers/ThemeController.kt
@@ -0,0 +1,39 @@
+package com.marchenkoteam.kotlinlearning.controllers
+
+import com.marchenkoteam.kotlinlearning.models.Theme
+import com.marchenkoteam.kotlinlearning.services.ThemeService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/theme")
+class ThemeController @Autowired constructor(private val themeService: ThemeService) {
+
+    @GetMapping
+    fun getThemes(): List<Theme> = themeService.findAll()
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PostMapping
+    fun saveTheme(@RequestHeader authToken: String,
+                  @RequestBody theme: Theme) {
+        themeService.save(theme)
+    }
+
+    @GetMapping("/{id}")
+    fun getTheme(@PathVariable("id") id: Long): Theme = themeService.findById(id)
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PutMapping("/{id}")
+    fun updateTheme(@RequestHeader authToken: String,
+                    @RequestBody theme: Theme) {
+        themeService.save(theme)
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/{id}")
+    fun deleteTheme(@RequestHeader authToken: String,
+                    @PathVariable("id") id: Long) {
+        themeService.deleteById(id)
+    }
+}
