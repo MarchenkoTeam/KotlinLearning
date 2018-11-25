d6e4d7c347c046e63371d782142ef8be44b94791
kiseru <kis.alex97@gmail.com>
2018-11-25 19:05:56 +0300

    Remove from GET /test/{id} field whoCompleted.

--- server-service/src/main/kotlin/com/marchenkoteam/kotlinlearning/controllers/TestController.kt
+++ server-service/src/main/kotlin/com/marchenkoteam/kotlinlearning/controllers/TestController.kt
@@ -1,5 +1,6 @@
 package com.marchenkoteam.kotlinlearning.controllers
 
+import com.marchenkoteam.kotlinlearning.dto.TestDto
 import com.marchenkoteam.kotlinlearning.models.Test
 import com.marchenkoteam.kotlinlearning.services.TestService
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,5 +14,5 @@ import org.springframework.web.bind.annotation.RestController
 class TestController @Autowired constructor(private val testService: TestService) {
 
     @GetMapping("{id}")
-    fun getTest(@PathVariable("id") id: Long): Test = testService.findById(id)
+    fun getTest(@PathVariable("id") id: Long): TestDto = testService.findById(id)
 }
